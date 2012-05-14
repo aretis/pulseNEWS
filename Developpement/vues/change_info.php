419552522ec74701673b59799953cd9d6517f629
Hoffmann Brice <hoffmann@intechinfo.fr>
2012-05-14 09:50:27 +0200

    nouveau fichiers
    Signed-off-by: Hoffmann Brice <hoffmann@intechinfo.fr>

--- Developpement/vues/change_info.php
+++ Developpement/vues/change_info.php
@@ -1,6 +1,7 @@
 ﻿<?php
 
 include ('/../modeles/profile_print.php');
+include ('/../modeles/call_db.php');
 	$connect = mysql_connect("localhost","root","")
 		or die("Connexion impossible : ".mysql_error());
 		mysql_select_db("pulsenews");
@@ -15,10 +16,26 @@ include ('/../modeles/profile_print.php');
 		$area_name = $req2[8];
 		$firstname = $req2[4];
 		$about = $req2[6];
-		$humor = $req[7];
+		$humor = $req2[7];
+		
 		
 		/*print_profile($key,$pseudo, $surname , $firstname ,$mail , $area_name , $about);*/
 ?>
+<?php if(isset($_POST['about_me'])) 
+{
+$id_user = 3;
+	echo " Vos informations ont bien été modifiés !";
+	$query = 'UPDATE users SET about = "'.$_POST['about_me'].'" WHERE id_user = '.$id_user;
+	if ( !mysql_query($query)) echo"La requete n'a pas aboutie";
+}?>
+<?php if(isset($_POST['humor'])) 
+{
+$id_user = 3;
+	echo " Vos informations ont bien été modifiés !";
+	$query = 'UPDATE users SET about = "'.$_POST['humor'].'" WHERE id_user = '.$id_user;
+	if ( !mysql_query($query)) echo"La requete n'a pas aboutie";
+}?>
+
 <div class='profile_ban'>
 	<img src='design/img/ban_exemple.png'/>
 </div>
@@ -33,65 +50,47 @@ include ('/../modeles/profile_print.php');
 <div class='profile_name'>&nbsp;<?php echo $pseudo;?>&nbsp;
 </td>
 <td>
-<a href='index.php?page=change_info'><div class='profile_button_left'>&nbsp;modifier mon profil&nbsp;</div></a>
+<div class='profile_button_left'>&nbsp;modifier mon profil&nbsp;</div>
 
 </td>
 </table>
 <table>
 <td>
-<table cellpadding='0' cellspacing='0' class='about_block'>
-			
-			<tr>
-				<td>
-					<img src='design/img/exemple_profile.jpg'/>
-				</td>
-			</tr>
-
-			<tr>
-				<td>
-					<div class='block_title_about'>&nbsp;à propos de moi</div>
-				</td>
-			</tr>
-			<tr>
-				<td style='background-color: #85c630;'>
-					<div class='block_content'>
-					<?php echo $about; ?> 				
-					</div>
-				</td>
-			</tr>
-			<tr style='height: 100%;'>
-			</tr>
-	</table>
-</td>
-<td>
 <table cellpadding='0' cellspacing='0' class='article'>
 <tr style='height: 10px;'>
 	<td rowspan='1'>
 	<div class='title_post'>
-		&nbsp;Nom du journal: Titre de l'article
+		&nbsp;Modifier mes infos : A propos de moi :
 	</div>
-	</td>
-
-	<td>
-		<div class='rate'>+128</div>
 	</td>
 </tr>
 <tr style='background-color: #85c630;'>
 	<td>
-		<div class='article_content'>
-		<br><img src='design/img/exemple_article.jpg'/><br>
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc tincidunt neque eget eros viverra tincidunt nec nec lacus. Mauris ullamcorper consequat dolor at sagittis. Nulla sed nunc semper lectus malesuada tristique et et sem. Vivamus at nisl velit, ut volutpat est. Nam a justo nibh. In consequat nunc id ante blandit in pellentesque turpis interdum. 
+		<div class='change_about_me'>
+		<form action='index.php?page=change_info' method='post'>
+		<br><input id='about' type='text' name ='about_me' VALUE ='function '><br>
+		<input type="submit" name="about" value=" Envois "/>
+		</form>
 		</div>
 	</td>
 </tr>
-<tr>
-<td>
-	<a href=''><div class='comment_button'>débattre</div></a>
-	<div style='float: right; width: 5%px;'>&nbsp;</div>
-	<a href=''><div class='rate_button'>DEpulse!</div></a>
-	<div style='float: right; width: 5%px;'>&nbsp;</div>
-	<a href=''><div class='rate_button'>PROpulse!</div></a>
-</td>
+</br>
+<tr style='height: 10px;'>
+	<td rowspan='1'>
+	<div class='title_post'>
+		&nbsp;Modifier mon humeur:
+	</div>
+	</td>
+</tr>
+<tr style='background-color: #85c630;'>
+	<td>
+		<div class='change_humor'>
+		<form action='index.php?page=change_info' method='post'>
+		<br><input id='humor' type='text' name ='humeur' VALUE ='<?php if(isset($_POST['humor'])) echo $_POST['humor']; ?>'><br>
+		<input type="submit" name="humor" value=" Envois  "/>
+		</form>
+		</div>
+	</td>
 </tr>
 <tr style='height: 100%'>
 </tr>
@@ -110,15 +109,13 @@ include ('/../modeles/profile_print.php');
 			<tr>
 				<td style='background-color: #85c630;'>
 				<div class='block_content'>
-					<?php
-						
-						?>
+					
 					<strong>Pseudo: </strong><?php echo $pseudo;?><br>
 					<strong>Nom: </strong><?php echo $surname;?><br>
 					<strong>Prénom: </strong><?php echo $firstname; ?><br>	
 					<strong>Mail: </strong><?php echo $mail; ?><br>
 					<strong>Région: </strong><?php echo $area_name; ?><br>
-					<strong>Humeur : </strong><?php echo $humor; ?><br>
+						<strong>humeur: </strong><?php echo $humor; ?><br>
 					</div>
 				</td>
 			</tr>
