eb4f8c2f04ecd7c79ef3fb34a60d68a0ab25dda1
Michel Gille <gille@intechinfo.fr>
2012-05-14 17:06:28 +0200

    Affichage news pulsées sur la page de profil + amélioration design
    Signed-off-by: Michel Gille <gille@intechinfo.fr>

--- Developpement/vues/profile.php
+++ Developpement/vues/profile.php
@@ -86,69 +86,117 @@
 
 	while($data = mysql_fetch_array($req))
 	{
-	
 		$content = $data['content'];
-		$id_post = $data['id_post'];
-	
-		echo"<table cellpadding='0' cellspacing='0' class='article'>";
-		echo"<tr style='height: 10px;'>";
-		echo"	<td rowspan='1'>";
-		echo"	<div class='title_post'>";
-		echo"		&nbsp;".$data['title'];
-		echo"	</div>";
-		echo"	</td>";
-		echo"";
-		echo"	<td>";
-		echo"		<div class='rate'>";
-		if($data['rate'] > 0) echo" + "; 
-		echo $data['rate']."</div>";
-		echo"	</td>";
-		echo"</tr>";
-		echo"<tr style='background-color: #85c630;'>";
-		echo"	<td>";
-		echo"		<div class='article_content'>";
-		echo"<p>";
-		echo $data['description'];
-		echo"</p>";
+		$id = $id_post = $data['id_post'];
 		
-		$id = $data['id_post'];
-		
-	
-		$request = "SELECT picture_id, picture_type, picture_blob FROM pictures WHERE post_id = $id";
-
-		$sucess = mysql_query ($request) or die (mysql_error ());
-		$col = mysql_fetch_assoc($sucess);
-			if ( !$col['picture_id'])
-			{
-				echo "Id d'image inconnu";
-			}
-			else
-			{
-				$image = imagecreatefromstring($col['picture_blob']);
-				ob_start(); //You could also just output the $image via header() and bypass this buffer capture.
-				imagejpeg($image, null, 80);
-				$data = ob_get_contents();
-				ob_end_clean();
-				echo '<br><img src="data:image/jpg;base64,' .  base64_encode($data)  . '" /><br>';
-			}
-			
-		$content = nl2br( $content , false );
-		echo $content;
+		if ($data['type'] == 1)
+		{
+		echo"		<table cellpadding='0' cellspacing='0' class='post_news' >";
+		echo"		<tr style='height: 32px;'>";
+		echo"		<td rowspan='1'>";
+		echo"		<div class='title_post'>";
+		echo"			&nbsp;".$data['title'];
 		echo"		</div>";
-		echo"	</td>";
-		echo"</tr>";
-		echo"<tr>";
-		echo"<td>";
-		echo"	<div class='debate'><form action='index.php?page=profile' method='POST'/><input type='submit' name='debattre' value='débattre' /></form></div>";
-		echo"	<div class='depulse'>&nbsp;";
-		echo"	<form action='index.php?page=profile' method='POST'/><input type='hidden' name='type' value='posts' /><input type='hidden' name='id_news' value='".$id."' /><input type='hidden' name='DEpulse' value='DEpulse' /><input type='submit' name='DEpulse' value='DEpulse' /></form></div></a>";
-		echo"	<div class='propulse'>&nbsp;";
-		echo"	<form action='index.php?page=profile' method='POST'/><input type='hidden' name='type' value='posts' /><input type='hidden' name='PROpulse' value='PROpulse' /><input type='hidden' name='id_news' value='".$id."' /><input type='submit' name='PROpulse' value='PROpulse' /></form></div></a>";
-		echo"</td>";
-		echo"</tr>";
-		echo"<tr style='height: 30px;'>";
-		echo"</tr>";
-		echo"</table>";
+		echo"		</td>";
+
+		echo"		<td>";
+		echo"			<div class='rate'>";
+		if($data['rate'] > 0) echo" + ";
+		echo $data['rate'];
+		echo "</div>";
+		echo"		</td>";
+		echo"	</tr>";
+		echo"	<tr style='background-color: #85c630;'>";
+		echo"		<td>";
+			echo"		<div class='description_news'>";
+			echo"			<a class='news_link' href='".$data['description']."'>&nbsp;&nbsp;&nbsp;lire l'article&nbsp;&nbsp;&nbsp;</a>&nbsp;&nbsp;";
+			echo "pulsé le : ".$data['post_date']." par 'michmich'</div>";
+			echo"	</td>";
+
+
+
+			echo"	<td style='background-color: white;'>";
+			echo"		<div class='date_news'>";
+				echo"	</div>";
+					
+				echo"</td>";
+
+			echo"</tr>";
+			echo"<tr>";
+			echo"	<td>";
+			echo"	<div class='debate'><form action='index.php?page=profile' method='POST'/><input type='submit' name='debattre' value='débattre' /></form></div>";
+			echo"	<div class='depulse'>&nbsp;";
+			echo"	<form action='index.php?page=profile' method='POST'/><input type='hidden' name='type' value='posts' /><input type='hidden' name='id_news' value='".$id."' /><input type='hidden' name='DEpulse' value='DEpulse' /><input type='submit' name='DEpulse' value='DEpulse' /></form></div></a>";
+			echo"	<div class='propulse'>&nbsp;";
+			echo"	<form action='index.php?page=profile' method='POST'/><input type='hidden' name='type' value='posts' /><input type='hidden' name='PROpulse' value='PROpulse' /><input type='hidden' name='id_news' value='".$id."' /><input type='submit' name='PROpulse' value='PROpulse' /></form></div></a>";
+			echo"</td>";
+			echo"</tr>";
+			echo"</table>";
+		}
+		else
+		{
+			
+		
+			echo"<table cellpadding='0' cellspacing='0' class='article'>";
+			echo"<tr style='height: 10px;'>";
+			echo"	<td rowspan='1'>";
+			echo"	<div class='title_post'>";
+			echo"		&nbsp;".$data['title'];
+			echo"	</div>";
+			echo"	</td>";
+			echo"";
+			echo"	<td>";
+			echo"		<div class='rate'>";
+			if($data['rate'] > 0) echo" + "; 
+			echo $data['rate']."</div>";
+			echo"	</td>";
+			echo"</tr>";
+			echo"<tr style='background-color: #85c630;'>";
+			echo"	<td>";
+			echo"		<div class='article_content'>";
+			echo"<p>";
+			echo $data['description'];
+			echo"</p>";
+			
+			$id = $data['id_post'];
+			
+		
+			$request = "SELECT picture_id, picture_type, picture_blob FROM pictures WHERE post_id = $id";
+
+			$sucess = mysql_query ($request) or die (mysql_error ());
+			$col = mysql_fetch_assoc($sucess);
+				if ( !$col['picture_id'])
+				{
+					echo "Id d'image inconnu";
+				}
+				else
+				{
+					$image = imagecreatefromstring($col['picture_blob']);
+					ob_start(); //You could also just output the $image via header() and bypass this buffer capture.
+					imagejpeg($image, null, 80);
+					$data = ob_get_contents();
+					ob_end_clean();
+					echo '<br><img src="data:image/jpg;base64,' .  base64_encode($data)  . '" /><br>';
+				}
+				
+			$content = nl2br( $content , false );
+			echo $content;
+			echo"		</div>";
+			echo"	</td>";
+			echo"</tr>";
+			echo"<tr>";
+			echo"<td>";
+			echo"	<div class='debate'><form action='index.php?page=profile' method='POST'/><input type='submit' name='debattre' value='débattre' /></form></div>";
+			echo"	<div class='depulse'>&nbsp;";
+			echo"	<form action='index.php?page=profile' method='POST'/><input type='hidden' name='type' value='posts' /><input type='hidden' name='id_news' value='".$id."' /><input type='hidden' name='DEpulse' value='DEpulse' /><input type='submit' name='DEpulse' value='DEpulse' /></form></div></a>";
+			echo"	<div class='propulse'>&nbsp;";
+			echo"	<form action='index.php?page=profile' method='POST'/><input type='hidden' name='type' value='posts' /><input type='hidden' name='PROpulse' value='PROpulse' /><input type='hidden' name='id_news' value='".$id."' /><input type='submit' name='PROpulse' value='PROpulse' /></form></div></a>";
+			echo"</td>";
+			echo"</tr>";
+			echo"<tr style='height: 30px;'>";
+			echo"</tr>";
+			echo"</table>";
+		}
 	}
 ?>
 
