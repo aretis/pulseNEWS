a383655d5712f064f3246c0b4a7a5df59791e22d
Hoffmann Brice <hoffmann@intechinfo.fr>
2012-05-14 18:36:38 +0200

    modification modele MVC pour change info dans la page profile
    Signed-off-by: Hoffmann Brice <hoffmann@intechinfo.fr>

--- Developpement/vues/profile.php
+++ Developpement/vues/profile.php
@@ -67,7 +67,7 @@
 			<tr>
 				<td style='background-color: #85c630;'>
 					<div class='block_content'>
-					<?php echo $about; ?> 				
+					<?php echo $about;?>				
 					</div>
 				</td>
 			</tr>
