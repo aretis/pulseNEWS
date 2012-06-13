353525d86f00da2f3020d9f1c34680b15f31a783
Alisha Bond <bunlon@intechinfo.fr>
2012-06-13 16:25:17 +0200

    Signed-off-by: Alisha Bond <bunlon@intechinfo.fr>

--- Developpement/modeles/chercher.php
+++ Developpement/modeles/chercher.php
@@ -1,5 +1,9 @@
 <html>
 <body>
+
+<link rel="stylesheet" href="design/register.css" />
+
+
 <form method="post" action="index.php?page=recherche">
 <input type="text"name="recherche"/>
 <select name ="mode">
@@ -8,15 +12,71 @@
 	<option value="un_mot">Au moins un mot</option>
 </select>
 
-<select name ="categorie">
+	 Catégorie<SELECT name="region">
+<?php
+	
+	$query = 'SELECT news_cat FROM cat_name';
+	$result = call_db($query);
+
+		while($donnees = mysql_fetch_array($result))
+		{
+			echo'<option>'.$donnees['categorie'].'</option>';
+		}
+	
+	mysql_free_result($result);
+	mysql_close($link);
+
+
+/*<select name ="categorie">
 	<option value="politique">politique</option>
 	<option value="Economie">Economie </option>
 	<option value="Faits_divers">Faits divers </option>
-	<option value="sport">sport</option>
-	<option value="culture">culture</option>
+	<option value="sport">Sport</option>
+	<option value="culture">Culture</option>
 	<option value="Ecologie">Ecologie</option>
 	</select>
 <input type="submit" value ="rechercher" name ="rechercher"/>
 	</form>
 
-</body></html>
+</body></html> */
+?>
+<html>
+<body>
+<br>
+<br>
+
+<link rel="stylesheet" href="design/register.css" />
+
+	<form action='index.php?page=modifier_compte' method='post' >
+	 Nom de l'utilisateur<input type="text" name="nom"/><br/>
+	 Prénom<input type="text" name="prenom"/><br/>
+	 nouveau mot de passe<input type="text" name="newPassword"/><br/>
+	 confirmation du mot de passe<input type="text" name="confirmPassword"/><br/>
+	 E-mail<input type="text" name="email"/><br/>
+	 Voulez vous modifier votre ville?<input type="checkbox" name="modif_ville[]" value="valid"/ ><br/>
+	 Ma région<SELECT name="region">
+<?php
+	
+	$query = 'SELECT area_name FROM AREAS';
+	$result = call_db($query);
+
+		while($donnees = mysql_fetch_array($result))
+		{
+			echo'<option>'.$donnees['area_name'].'</option>';
+		}
+	
+	mysql_free_result($result);
+	mysql_close($link);
+?>
+  </SELECT>
+  
+<br><br>
+	
+	supprimer mon compte &nbsp; <INPUT type="checkbox" name="delete_profil">
+
+	<input type='submit'  value="envoyer"/>
+
+</form>
+
+</body>
+</html>
