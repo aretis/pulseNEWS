00cfc20e7ede07de21a29954eef34b1c7f368b25
Michel Gille <gille@intechinfo.fr>
2012-06-24 19:10:10 +0200

    Signed-off-by: Michel Gille <gille@intechinfo.fr>

--- Developpement/modeles/comment.php
+++ Developpement/modeles/comment.php
@@ -15,11 +15,19 @@
 	$query = "SELECT id_comment, content, post_date, pseudo FROM comments INNER JOIN USERS ON comments.id_user = users.id_user WHERE id_post = ".$id;
 	$result = call_db($query);
 
+
+	?>
+	
+		<div id="wrapper">
+		<div class="accordionButton">Commentaires</div>
+		<div class="accordionContent">
+		
+	<?php
 	while($data = mysql_fetch_array($result))
 	{
 		
 		echo"<div class='comment_post'>";
-		echo"<span style='font-size:22px; color:white; font-weight:bold;'>";
+		echo"<span style='font-size:22px;  font-weight:bold;'>";
 			if(isset($_SESSION['pseudo']))
 		{
 			if($data['pseudo'] == $_SESSION['pseudo'])
@@ -36,11 +44,20 @@
 		echo "&nbsp;&nbsp;".$data['content'];
 		echo"</span>";
 		
-		echo"<span style='font-size:10px; color:white;'>";
+		echo"<span style='font-size:10px;'>";
 		echo"<br>&nbsp;&nbsp;Ecrit le ";
 		echo date("d/m/Y à H\hi", strtotime($data['post_date']));
 		echo"</div>";
 
-		echo"</span><hr>";
+		echo"</span>";
 	}
-?>
+	
+
+	?>
+	
+	
+	
+		</div>
+
+	</div>
+	
