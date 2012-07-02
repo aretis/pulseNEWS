916ff53056a357462597e516191a108b62a0a159
Michel Gille <gille@intechinfo.fr>
2012-07-02 15:31:47 +0200

    Signed-off-by: Michel Gille <gille@intechinfo.fr>

--- Developpement/controleurs/create_article.php
+++ Developpement/controleurs/create_article.php
@@ -4,40 +4,47 @@ include_once('modeles/call_db.php');
 include_once('modeles/create_article.php');
 
 $field_errors = 0;
+$no_image = 0;
 
-if( isset($_POST['area']) || isset($_POST['cat']) )
+if( isset($_FILES['fichier']['tmp_name']) && empty($_FILES['fichier']['tmp_name']))
 {
-
-	
-	if( !empty($_POST['title']) && isset($_FILES['fichier']) && !empty($_POST['description']) && !empty($_POST['content']) && !empty($_POST['area']) && !empty($_POST['cat']))
-	{
-		
-	
-		$article = array();
-
-		$article['title'] = $_POST['title'];
-		$article['description'] = $_POST['description'];
-		$article['content'] = $_POST['content'];
-		$article['area'] = $_POST['area'];
-		$article['cat'] = $_POST['cat'];
-		
-		create_article($article);
-		
-		header('Location:index.php?page=profile');
-	}
-	else
-	{
-		$field_errors = 1;
-		$article['title'] = $_POST['title'];
-		$article['description'] = $_POST['description'];
-		$article['content'] = $_POST['content'];
-		$article['area'] = $_POST['area'];
-		$article['cat'] = $_POST['cat'];
-	}
-	
-	
+	$no_image = 1;
 }
+else
+{
+	if( isset($_POST['area']) || isset($_POST['cat']) )
+	{
 
+		
+		if( !empty($_POST['title']) && isset($_FILES['fichier']) && !empty($_POST['description']) && !empty($_POST['content']) && !empty($_POST['area']) && !empty($_POST['cat']))
+		{
+			
+		
+			$article = array();
+
+			$article['title'] = $_POST['title'];
+			$article['description'] = $_POST['description'];
+			$article['content'] = $_POST['content'];
+			$article['area'] = $_POST['area'];
+			$article['cat'] = $_POST['cat'];
+			
+			create_article($article);
+			
+			header('Location:index.php?page=profile');
+		}
+		else
+		{
+			$field_errors = 1;
+			$article['title'] = $_POST['title'];
+			$article['description'] = $_POST['description'];
+			$article['content'] = $_POST['content'];
+			$article['area'] = $_POST['area'];
+			$article['cat'] = $_POST['cat'];
+		}
+		
+		
+	}
+}
 
 
 include('vues/header.php');
